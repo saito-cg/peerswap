10d1534cedaf741b250b612ffabf3a077a7fef70
saito-cg <124134085+saito-cg@users.noreply.github.com>
2025-12-16 10:25:40 +0900

    add debug log

--- lwk/lwkwallet.go
+++ lwk/lwkwallet.go
@@ -127,12 +127,15 @@ func (r *LWKRpcWallet) setupWallet(ctx context.Context) error {
 
 func (r *LWKRpcWallet) createWallet(ctx context.Context, walletName, signerName string) error {
 	fmt.Printf("[DEBUG] LWK Version: %s", r.lwkVersion)
-	res, err := r.lwkClient.generateSigner(ctx)
+	_, err := r.lwkClient.generateSigner(ctx)
 	if err != nil {
 		return err
 	}
+
+	mnemonic := "dog door office perfect lazy cloud call film eight age easy bone"
+	log.Infof("検証のため、固定のニーモニックを使用しています: %s", mnemonic)
 	_, err = r.lwkClient.loadSoftwareSigner(ctx, &loadSoftwareSignerRequest{
-		Mnemonic:   res.Mnemonic,
+		Mnemonic:   mnemonic,
 		SignerName: signerName,
 		Persist:    true,
 	})
